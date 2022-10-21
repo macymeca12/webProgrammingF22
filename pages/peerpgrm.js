7c2cc095b17a964f9be8ecb7b2e7503de377cb0f
macymeca12 <marianny4641@hotmail.com>
2022-10-21 00:41:20 -0400

    style on random button

--- pages/peerpgrm.js
+++ pages/peerpgrm.js
@@ -1,12 +1,3 @@
-// var button = document.getElementById("button1");
-// var artist = [
-//   "http://www.juliascher.com/openthedoor/index.html",
-//   "https://www.benkinmont.com/#",
-//   "https://projects.jennyholzer.com/",
-//   "http://adaweb.walkerart.org/project/blindspot/",
-//   "http://adaweb.walkerart.org/context/artists/weiner/weiner0.html",
-// ];
-
 var randomlinks = new Array();
 
 randomlinks[0] = "http://www.juliascher.com/openthedoor/index.html";
@@ -19,11 +10,3 @@ randomlinks[4] =
 function randomlink() {
   window.location = randomlinks[Math.floor(Math.random() * randomlinks.length)];
 }
-
-// $( "#clickme" ).click(function() {
-//     $("#displayme").html(list[Math.floor(Math.random()*list.length)]);
-//   });
-
-// button.onclick = function () {
-//   alert(artist[Math.floor(Math.random() * artist.length)]);
-// };
